8f323dccc061f22d1faf5507207ed577f6c2da36
ncampise2655@floridapoly.edu <ncampise2655@floridapoly.edu>
2025-06-09 17:08:16 -0400

    latest

---
+++ rust_basics/data_types/src/compound/mod.rs
@@ -0,0 +1,9 @@
+// Compound types can group multiple values into one type. Rust has two primitive compound types: tuples and arrays.
+
+pub fn tuples() {
+    println!("Write Here!");
+}
+
+pub fn array() {
+    println!("Write Here!");
+}
--- rust_basics/data_types/src/main.rs
+++ rust_basics/data_types/src/main.rs
@@ -1,46 +1,38 @@
 use std::env;
+mod compound;
 mod scalar;
-mod tuple;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-// Scalar
-//
-// fn main() {
-//     // list of all arguments passed to the binary
-//     let args: Vec<String> = env::args().collect();
-//     // first argument
-//     // TODO allow for handling of arguments
-//     let query = args.get(1).expect("Expected first argument").to_lowercase();
-//     if query == "scalar" {
-//         println!("It worked");
-//     } else if query == "vector" {
-//         println!("vector");
-//     } else if query == "string" {
-//         println!("string");
-//     } else if query == "tuple" {
-//         println!("tuple");
-//     } else {
-//         panic!("Expected first argument of either scalar, vector, string, or tuple");
-//     }
-// }
-
-/// Simple program to greet a person
+// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// Name of the person to greet
     #[arg(short, long)]
-    name: String,
+    print: String,
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Write {
+        /// Optionally set a title for what you are going to write about
+        #[clap(short, long)]
+        title: Option<String>,
+    },
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    match args.print.as_str() {
+        "int" => scalar::int(),
+        "float" => scalar::float(),
+        "sum" => scalar::sum(),
+        "difference" => scalar::difference(),
+        "multiply" => scalar::multiply(),
+        "divide" => scalar::divide(),
+        "bool" => scalar::bool(),
+        "char" => scalar::char(),
+        _ => println!("command was not found"),
     }
 }
